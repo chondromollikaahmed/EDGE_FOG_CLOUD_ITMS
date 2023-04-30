73f8a457a3b99469e5a82961b6f97d1204a932f3
ChondromollikaAhmed <chondromollikaahmed@gmail.com>
2023-05-01 05:16:34 +0600

    new file:   V3/merged/merged.ino 	new file:   V3/merged/merged_fog.ino

---
+++ V3/merged/merged_fog.ino
@@ -0,0 +1,257 @@
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <esp_now.h> 
+#else
+  #include <ESP8266WiFi.h>
+  #include <espnow.h>
+#endif
+
+
+
+#define NUMOFNODES 20
+esp_now_peer_info_t nodes[NUMOFNODES] = {};
+int nodeCount = 0;
+
+
+#define CHANNEL 1
+#define PRINTSCANRESULTS 0
+
+
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+
+
+String data;
+
+// Scan for Nodes in Access Point mode
+void ScanForNode() {
+  data="";
+  int8_t scanResults = WiFi.scanNetworks();
+  //reset nodes
+  memset(nodes, 0, sizeof(nodes));
+  nodeCount = 0;
+  if (scanResults == 0) { Serial.println("No WiFi devices in AP Mode found");} 
+  else
+   {
+    for (int i = 0; i < scanResults; ++i) {
+      // Print SSID and RSSI for each device found
+      String SSID = WiFi.SSID(i);
+      int32_t RSSI = WiFi.RSSI(i);
+      String BSSIDstr = WiFi.BSSIDstr(i);
+
+      if (PRINTSCANRESULTS) {
+        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+      }
+      delay(10);
+
+      // Check if the current device starts with `Node`  
+      if (SSID.indexOf("Node") == 0) {
+        // SSID of interest
+        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+        // Get BSSID => Mac Address of the Edge
+        int mac[6];
+
+        data+="Node:"+BSSIDstr;
+
+        if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
+          for (int j = 0; j < 6; ++j ) {
+            nodes[nodeCount].peer_addr[j] = (uint8_t) mac[j];
+
+          }
+        }
+        nodes[nodeCount].channel = CHANNEL; // pick a channel
+        nodes[nodeCount].encrypt = 0; // no encryption
+        nodeCount++;
+      }
+    }
+  }
+
+  if (nodeCount > 0) {
+    Serial.print(nodeCount); Serial.println(" Nodes(s) found, processing..");
+  } else {
+    Serial.println("No Nodes Found, trying again.");
+  }
+  // clean up ram
+  WiFi.scanDelete();
+}
+
+
+
+
+
+
+// Check if the edge is already paired with the fog.
+// If not, pair the edge with the fog
+void manageNode() {
+  if (nodeCount > 0) {
+    for (int i = 0; i < nodeCount; i++) {
+      Serial.print("Processing: ");
+      for (int ii = 0; ii < 6; ++ii ) {
+        Serial.print((uint8_t) nodes[i].peer_addr[ii], HEX);
+        if (ii != 5) Serial.print(":");
+      }
+      Serial.print(" Status: ");
+      // check if the peer exists
+      bool exists = esp_now_is_peer_exist(nodes[i].peer_addr);
+      if (exists) {
+        // Edge already paired.
+        Serial.println("Already Paired");
+      } else {
+        // Edge not paired, attempt pair
+        esp_err_t addStatus = esp_now_add_peer(&nodes[i]);
+        if (addStatus == ESP_OK) {
+          // Pair success
+          Serial.println("Pair success");
+        } else if (addStatus == ESP_ERR_ESPNOW_NOT_INIT) {
+          Serial.println("ESPNOW Not Init");
+        } else if (addStatus == ESP_ERR_ESPNOW_ARG) {
+          Serial.println("Add Peer - Invalid Argument");
+        } else if (addStatus == ESP_ERR_ESPNOW_FULL) {
+          Serial.println("Peer list full");
+        } else if (addStatus == ESP_ERR_ESPNOW_NO_MEM) {
+          Serial.println("Out of memory");
+        } else if (addStatus == ESP_ERR_ESPNOW_EXIST) {
+          Serial.println("Peer Exists");
+        } else {
+          Serial.println("Not sure what happened");
+        }
+        delay(100);
+      }
+    }
+  } else {
+    // No Edge found to process
+    Serial.println("No Edge found to process");
+  }
+}
+
+
+
+
+void sendData() {
+  for (int i = 0; i < nodeCount; i++) {
+    const uint8_t *peer_addr = nodes[i].peer_addr;
+    esp_err_t result = esp_now_send(peer_addr, (const uint8_t *)data.c_str(), data.length());
+    
+    Serial.print("Send Status: ");
+    if (result == ESP_OK) {
+      Serial.println("Success");
+    } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+      // How did we get so far!!
+      Serial.println("ESPNOW not Init.");
+    } else if (result == ESP_ERR_ESPNOW_ARG) {
+      Serial.println("Invalid Argument");
+    } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+      Serial.println("Internal Error");
+    } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+      Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+    } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+      Serial.println("Peer not found.");
+    } else {
+      Serial.println("Not sure what happened");
+    }
+    delay(100);
+  }
+}
+
+void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength)
+// Formats MAC Address
+{
+  snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+}
+
+
+
+void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen)
+// Called when data is received
+{
+  // Only allow a maximum of 250 characters in the message + a null terminating byte
+  char buffer[ESP_NOW_MAX_DATA_LEN + 1];
+  int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
+  strncpy(buffer, (const char *)data, msgLen);
+
+  // Make sure we are null terminated
+  buffer[msgLen] = 0;
+
+  // Format the MAC address
+  char macStr[18];
+  formatMacAddress(macAddr, macStr, 18);
+
+  // Send Debug log message to the serial port
+  Serial.printf("Received message from: %s - %s\n", macStr, buffer);
+
+  String msg ;
+  msg=buffer;
+  
+
+}
+
+
+
+
+
+// callback when data is sent from FOG to Edge
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
+  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+
+
+
+
+
+void setup() {
+
+
+
+   Serial.begin(115200);
+  //Set device in STA mode to begin with
+  WiFi.mode(WIFI_STA);
+  Serial.println("Fog Device Will Run In Station Mode");
+  // This is the mac address of the FOG in Station Mode
+  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(receiveCallback);
+ 
+
+}
+
+void loop() {
+  ScanForNode();
+  // If Edge is found, it would be populate in `Edge` variable
+  // We will check if `Edge` is defined and then we proceed further
+  if (nodeCount > 0) { // check if Edge channel is defined
+    // `Edge` is defined
+    // Add Edge as peer if it has not been added already
+    manageNode();
+    // pair success or already paired
+    // Send data to device
+    sendData();
+  } else {
+    // No Edge found to process
+  }
+
+  // wait for 3seconds to run the logic again
+  delay(1000);
+
+}
